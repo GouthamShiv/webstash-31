ceabbb46e771aa4584f349072dd57b9fee658ce7
Goutham S <gouthamshine@gmail.com>
2022-01-13 13:17:00 +0530

    generate random password

--- app/script.js
+++ app/script.js
@@ -1,9 +1,31 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateBtn = document.getElementById('generate');
+const clipboardBtn = document.getElementById('clipboard');
+
 const randomFunction = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol,
 }
+
+const minLength = 4;
+const maxLength = 20;
+
+generateBtn.addEventListener('click', () => {
+    const length = +lengthEl.value > 20 ? 20 : +lengthEl.value < 4 ? 4 : +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
+})
 
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -20,4 +42,23 @@ function getRandomNumber() {
 function getRandomSymbol() {
     const symbols = '!@#$%^&*(){}[]=<>/,.';
     return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePassword(length, lower, upper, number, symbol) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+
+    if (typesCount === 0) {
+        return '';
+    }
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunction[funcName]();
+        })
+    }
+    const randomPassword = generatedPassword.slice(0, length);
+    return randomPassword;
 }
